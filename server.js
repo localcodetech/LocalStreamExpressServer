d7521352e4fe3263027134cdf3bb74e64e88a730
localcodetech <rbphraim95@gmail.com>
2026-09-10 19:35:40 +0000

    added server

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 import app from "./src/init.js";
 import { config } from "dotenv";
 config();
+import sequelize from "./src/config/dbConnection.js";
+import {writeFile, readFile} from "fs/promises";
 
 
 const {PORT} = process.env;
-
+const ERROR_FILE = "logs/logging.txt"
 
 
 const main =async ()=>{
-
+    console.log("... \n STARTING SERVER .... PLEASE WAIT .... \n ")
 
     try{
+        await sequelize.authenticate()
+
+        console.log("...  DATABASE CONNECTED SUCCESSFULLY ... ")
 
 
         app.use("/", (_,res)=>{
@@ -18,11 +23,11 @@ const main =async ()=>{
         })
         app.listen(PORT,
             ()=>{
-                console.log("")
+                console.log(`\n express is running on http://127.0.0.1:${PORT} \n`)
             }
         )
     }catch(error){
-
+            await  writeFile(ERROR_FILE,error.message, {mode: '007'} )
     }
 
 
---
+++ src/models/channelModels.js
@@ -0,0 +1,51 @@
+import { truncates } from "bcryptjs";
+import sequelize from "../config/dbConnection.js";
+import { DataTypes } from "sequelize";
+
+
+
+const Channel = sequelize.define("Channel", {
+
+    id : {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+    },
+    uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        unique: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    streamUrl: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category : {
+        type: DataTypes.STRING,
+        defaultValue : "all"
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+
+    },
+    isLive: {
+        type: DataTypes.STRING
+    }
+})
+
+
+
+
+export default sequelize;
