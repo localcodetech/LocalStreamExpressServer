5b243807a646f6d6f3db0db98e5eceeadbbfe45d
localcodetech <rbphraim95@gmail.com>
2026-09-10 21:58:20 +0000

    dev

--- src/init.js
+++ src/init.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(morgan("combined")));
 
 
 
