7e5f4422c413c4ef4afa4607a39eda8d72c30cf4
localcodetech <rbphraim95@gmail.com>
2026-09-10 21:53:12 +0000

    fix

--- src/repositories/channelRepositories.js
+++ src/repositories/channelRepositories.js
@@ -5,3 +5,33 @@ import Channel from "../models/channelModels.js";
 
 // QUERIES HERE 
 
+export const findAllChannel = async () => {
+    return await Channel.findAll();
+};
+
+
+export const findByName = async (name) =>{
+    return await Channel.findOne({where: {name: name}})
+}
+
+
+export const findByPk = async  (id) =>{
+    return await Channel.findByPk(id)
+}
+
+
+export const findByType = async (type) =>{
+    return await Channel.findOne({where: {type: type}});
+};
+
+export const findByCategory = async (category) =>{
+    return await Channel.findOne({where: {category:category }})
+}
+
+
+
+// create 
+
+export const createNewChannel = async ()=>{
+    
+}
