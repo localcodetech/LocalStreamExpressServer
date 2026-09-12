ddd7f5934558a55e78ac6ff348f8f97566bca455
localcodetech <rbphraim95@gmail.com>
2026-09-12 17:48:26 +0000

    error prone

--- src/init.js
+++ src/init.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import router from "./routes/channelRoutues.js";
 
 
 const app = express();
 app.use(express.json());
-app.use(cors(morgan("combined")));
+app.use(cors());
+app.use(morgan("combined"));
+app.use("/api",router )
+
 
 
 
---
+++ src/routes/channelRoutues.js
@@ -0,0 +1,17 @@
+import { registerNewChannel } from "../controllers/channelControllers.js";
+
+import { Router } from "express";
+
+    const router  = Router();
+    const postController = await registerNewChannel()
+
+
+    router.post("/channel", postController);
+
+    router.get("/health", (_,res)=>{res.status(200).json({message: "Channel Route is reachable"})})
+
+ 
+    
+    export default router;
+
+
