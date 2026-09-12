3ebca772df3f8aac3e54eb1a3ab27685b188beec
localcodetech <rbphraim95@gmail.com>
2026-09-12 09:42:25 +0000

    fix

--- src/schemas/channelSchema.js
+++ src/schemas/channelSchema.js
@@ -4,5 +4,10 @@ import {z as zod} from "zod"
 
 const schema = zod.object({
     name: zod.string().trim().min(3),
-    category : zod.enum([])
+    category : zod.enum(["all", "news", "gospel", "music", "sports", "movies", "documentary"]),
+    type : zod.enum(["radio", "tv"]),
+    streamUrl: zod.url(),
+    description : zod.string().trim().optional(),
+    logo: zod.url(),
+    
 })
